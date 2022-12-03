a1a3c8cd737593521638ed8cbedcddff85a440ae
Tobias Binna <tobias.binna@toolsplus.io>
2022-12-03 12:20:11 +0800

    Update to Nx Forge 1.3.0
    - Update to Nx Forge 1.3.0
    - Upgrade Nx to latest version

--- apps/todo-panel/jest.config.ts
+++ apps/todo-panel/jest.config.ts
@@ -1,9 +1,10 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'todo-panel',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/todo-panel',
